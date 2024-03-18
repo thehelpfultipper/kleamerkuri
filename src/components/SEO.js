094ecf2f9f5cf5ccfd741a95fa329b23893d87eb
THT <thehelpfultipper@gmail.com>
2024-03-18 08:37:15 -0700

    Updating seo component with site image and url

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -3,7 +3,14 @@ import React from 'react';
 import useSiteMetadata from '../hooks/use-sitemetadata';
 
 export default function SEO({ title, description, children }) {
-  const { description: siteDesc, title: siteTitle, author, keywords } = useSiteMetadata();
+  const { 
+    description: siteDesc, 
+    title: siteTitle, 
+    author, 
+    keywords,
+    siteUrl,
+    siteImage
+  } = useSiteMetadata();
 
   const seo = {
     title: `${title} - ${siteTitle}` || siteTitle,
@@ -16,8 +23,11 @@ export default function SEO({ title, description, children }) {
       <meta name="author" content={ author } />
       <meta name="keywords" content={ keywords } />
       <meta name="og:title" content={ seo.title } />
+      <meta name="og:url" content={ siteUrl } />
+      <meta name="og:image" content={ siteImage } />
       <meta name="og:description" content={ seo.description } />
       <meta name="og:type" content="website" />
+      <link rel="icon" href="/favicon.ico" />
       <title>{ seo.title }</title>
       { children }
     </>
