37a173ca2ceadbad2e604556715e717870c503fe
THT <thehelpfultipper@gmail.com>
2024-03-18 17:12:15 -0700

    Updates

--- sw.js
+++ sw.js
@@ -36,11 +36,11 @@ self.__precacheManifest = [
     "url": "framework-f233066e4c86ecfb2184.js"
   },
   {
-    "url": "app-9d84486ddfd1f2905284.js"
+    "url": "app-130d21e77cc72b6ea6f9.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "41e2bdd5c71e08aa5f92121501a741b0"
+    "revision": "3336f0a1da5ee511029d985941e067eb"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -150,7 +150,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/kleamerkuri/app-9d84486ddfd1f2905284.js`))) {
+  if (!resources || !(await caches.match(`/kleamerkuri/app-130d21e77cc72b6ea6f9.js`))) {
     return await fetch(event.request)
   }
 
