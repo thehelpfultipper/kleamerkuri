666ad48169b99a9aea9b08184ce04d02a5f45e38
THT <thehelpfultipper@gmail.com>
2024-03-19 08:27:05 -0700

    Adjust filter styles for projects mobile

--- src/components/ProjectsFilter.js
+++ src/components/ProjectsFilter.js
@@ -28,7 +28,7 @@ const ToggleButton = styled(Button)({
     },
     '@media (max-width: 545px)': {
         position: 'static',
-        maxWidth: '145px',
+        maxWidth: '80%',
         width: '100%',
         height: '100%',
         opacity: '1',
